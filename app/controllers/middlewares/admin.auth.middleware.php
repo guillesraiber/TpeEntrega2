7d5566827c976ca97a01300efdd71a11532de651
guillesraiber <143103497+guillesraiber@users.noreply.github.com>
2024-11-14 11:18:24 -0300

    Delete app directory

--- app/controllers/middlewares/admin.auth.middleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-function adminAuthMiddleware($res) {
-    if (!isset($res->user) || $res->user->role !== 'admin') {
-        header('Location: ' . BASE_URL . 'showLogin');
-        die();
-    }
-}
-
-
